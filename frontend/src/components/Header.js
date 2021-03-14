40ad4c73bd2fec737a5f982802b70c201e6144e8
Varun Nandyal <varun.nandyal@gmail.com>
2021-03-13 19:12:32 -0600

    Add frontend files

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img
+          src="https://logrocket-assets.io/img/logo.png"
+          width="300"
+          className="img-thumbnail"
+          style={{ marginTop: "20px" }}
+          alt="Logo"
+        />
+        <hr />
+        <h5>
+          <i>presents</i>
+        </h5>
+        <h1>App with React + Django</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,1 @@
+export const API_URL = "http://192.168.86.34:8000/api/students/";
